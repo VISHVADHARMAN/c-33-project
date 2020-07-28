3780beb5fa5757d66a0aa0d5aede970026c7c7ff
VISHVADHARMAN <67373113+VISHVADHARMAN@users.noreply.github.com>
2020-07-28 19:42:51 +0530

    all files are uploaded

---
+++ js/line.js
@@ -0,0 +1,16 @@
+class Line
+{
+    constructor(x,y,w,h) 
+    {
+      var options = 
+      {
+          isStatic: true
+      }
+    }
+    display()
+    {
+    rectMode(CENTER);
+    fill("yellow");
+    rect(600, 480, 1200, 5);
+    }
+  };
